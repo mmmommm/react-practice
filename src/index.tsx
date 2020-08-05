abf120a151ce920137fb92433cd8fc8022c3e2ca
ryota kise <ryota1839@gmail.com>
2020-08-05 16:14:33 +0900

    クリックしたところがxになる

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+type SquareType = string | null;
+
+interface BoardState {
+  squares: SquareType[];
+}
+
+interface SquareProps {
+  value: SquareType;
+  onClick: () => void;
+}
+
+function Square(props: SquareProps) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
+
+class Board extends React.Component<any, BoardState> {
+  constructor(props: BoardState) {
+    super(props);
+    this.state = {
+      squares: Array<SquareType>(9).fill(null),
+    };
+  }
+  handleClick(i: number) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X'
+    this.setState({squares: squares})
+  }
+  renderSquare(i: number) {
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+  }
+  render() {
+    const status = 'Next player: X';
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
